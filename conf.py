3506649be82e14939397dc7a567df33adf8e1317
Florian Posdziech <hallo@flowfx.de>
2019-02-13 07:44:59 +0100

    Configure to hide future and draft posts

--- conf.py
+++ conf.py
@@ -923,14 +923,14 @@ PRETTY_URLS = True
 
 # If True, publish future dated posts right away instead of scheduling them.
 # Defaults to False.
-FUTURE_IS_NOW = True
+FUTURE_IS_NOW = False
 
 # If True, future dated posts are allowed in deployed output
 # Only the individual posts are published/deployed; not in indexes/sitemap
 # Generally, you want FUTURE_IS_NOW and DEPLOY_FUTURE to be the same value.
-DEPLOY_FUTURE = True
+DEPLOY_FUTURE = False
 # If False, draft posts will not be deployed
-DEPLOY_DRAFTS = True
+DEPLOY_DRAFTS = False
 
 # Allows scheduling of posts using the rule specified here (new_post -s)
 # Specify an iCal Recurrence Rule: http://www.kanzaki.com/docs/ical/rrule.html
