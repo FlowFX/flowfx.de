3a9abd66e31d3eebd5fe6d11c49b4368d1491496
Florian Posdziech <hallo@flowfx.de>
2020-03-11 13:37:23 +0100

    Make it red

--- conf.py
+++ conf.py
@@ -1165,7 +1165,7 @@ USE_OPEN_GRAPH = True
 # If webassets is installed, bundle JS and CSS into single files to make
 # site loading faster in a HTTP/1.1 environment but is not recommended for
 # HTTP/2.0 when caching is used. Defaults to True.
-USE_BUNDLES = True
+USE_BUNDLES = False
 
 # Plugins you don't want to use. Be careful :-)
 # DISABLED_PLUGINS = ["render_galleries"]
